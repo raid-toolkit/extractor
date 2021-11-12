52cdca95e8fd63deb20cfa385355856a65c62720
Daniel Chattan <dnchattan@gmail.com>
2021-11-12 08:53:03 -0500

    re-add stathread

--- Program.cs
+++ Program.cs
@@ -56,6 +56,7 @@ namespace RaidExtractor
             Application.Run(new MainForm());
         }
 
+        [STAThread]
         static void Main(string[] args)
         {
             if (args.Length == 0)
